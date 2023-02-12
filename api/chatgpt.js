70cb3148a197e06332356f72e362a2dca9d9ad0e
shashank <shashanktrivedi1917@gmail.com>
2023-02-13 00:57:13 +0530

    initial

---
+++ api/chatgpt.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+require("dotenv").config();
+const CHATGPT_API_KEY = process.env.CHATGPT_API_KEY;
+// app.use(express.json());
+const router = express.Router();
+
+router.post("/answer", async (req, res, next) => {
+  const { prompt } = req.body;
+  console.log(prompt);
+  res.send("dine");
+  return 0;
+  const response = await axios.post(
+    "https://api.openai.com/v1/completions",
+    {
+      prompt: req.body.text,
+      max_tokens: 1000,
+      n: 1,
+      stop: null,
+      temperature: 0.5,
+      // max_tokens: 7,
+      model: "text-davinci-003",
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${CHATGPT_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const generatedText = response.data.choices[0].text;
+  console.log("ye hai");
+  console.log(generatedText);
+  res.send({ generatedText });
+});
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const answer = require("./api/chatgpt");
+// const sendData = require("./api/bu");
+app.get("/", (req, res) => {
+  res.json("Hello, World!");
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+
+  // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
+  //   res.setHeader("Access-Control-Allow-Credentials", true);
+
+  next();
+});
+
+app.use(answer);
+
+app.listen(process.env.PORT || 3001, () => {
+  console.log("Server is listening on port 3001");
+});
